e78e80bac08df540528d4056f45edb5fb9166bb8
Stosh Fabricius <stosh.fabricius@gmail.com>
2018-04-25 07:49:49 -0400

    Update example 3 in W03d2 breakout

--- w03-d2-breakout/code/example_3/main.js
+++ w03-d2-breakout/code/example_3/main.js
@@ -1,32 +1,41 @@
 
-var pets = [
-  {
-    name: 'Dingo',
-    type: 'dog',
-    age: 1
-  },
-  {
-    name: 'Kongo',
-    type: 'snake',
-    age: 3
-  },
-  {
-    name: 'Meowngo',
-    type: 'cat',
-    age: 2
-  },
-  {
-    name: 'Bongo',
-    type: 'dog',
-    age: 2
-  }
-]
+
+
+
+function fetchPets() {
+  var pets = [
+    {
+      name: 'Dingo',
+      type: 'dog',
+      age: 1
+    },
+    {
+      name: 'Kongo',
+      type: 'snake',
+      age: 3
+    },
+    {
+      name: 'Meowngo',
+      type: 'cat',
+      age: 2
+    },
+    {
+      name: 'Bongo',
+      type: 'dog',
+      age: 2
+    }
+  ]
+  return pets
+}
 
 $('.show-pets-btn').on('click', function(event) {
   // Append a list of pets to the pets container
   console.log('Show all the pets')
   var $petsContainer = $('#pets-container')
 
+  // Tomorrow we wil use AJAX to fetch them
+  var pets = fetchPets()
+  
   // Pets was available globally
   pets.forEach(function(item) {
     // Create a new p tag
