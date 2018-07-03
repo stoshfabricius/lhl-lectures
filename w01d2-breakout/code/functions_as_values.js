302106e5d7e9d9edda14e88601ae036c70983810
Stosh Fabricius <stosh.fabricius@gmail.com>
2018-07-03 14:34:16 -0400

    Update example in w01d2-breakout

--- w01d2-breakout/code/functions_as_values.js
+++ w01d2-breakout/code/functions_as_values.js
@@ -1,5 +1,6 @@
 // Function declared as an expression
-// the function is anonymous as the function does not have a name
+// The function is anonymous as the function does not have a name
+// However, a name can be provided with a function expression and can be used inside the function to refer to itself, or in a debugger to identify the function in stack traces:
 // These are convenient when passing a function as an argument to another function
 var add = function(a,b) {
   return a + b
@@ -7,12 +8,12 @@ var add = function(a,b) {
 console.log('Adding 1 + 2', add(1,2))
 
 
-// Function declared using function definition
+// Function declared using function definition (also called function declaration, or function statement )
 function subtract(a, b) {
   return a - b
 }
 
-console.log('Subtracting 3 -2 ',subtract(3,2))
+console.log('Subtracting 3 -2 ', subtract(3,2))
 
 // What happens if we move the console.log above the function definition
 
--- w01d2-breakout/code/readingCode.js
+++ w01d2-breakout/code/readingCode.js
@@ -2,14 +2,12 @@
 var x = 2;
 var y = 3;
 
+// Will this work?
 console.log('What is ' + x + '+' +  y + ' ?');
 console.log('It is: ', add(x,y))
 
-// Why does this work
-function add(x,y) {
-  return x + y;
-}
 
+// Load code from other files
 // var subtract = require('./subtract.js');
 
 // var a = 3;
@@ -21,7 +19,14 @@ function add(x,y) {
 // console.log('Look how good we are at math!')
 
 
+// Control flow of the program using loops
+
 // console.log('Look how many times I can loop', i)
 // for (var i = 0; i < 10, i++) {
 //   console.log('Loop', i)
 // }
+
+
+function add(x,y) {
+  return x + y;
+}
