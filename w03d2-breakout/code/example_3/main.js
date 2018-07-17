6e15c4f50335678f1317376cfa8efe3477f767d8
Stosh Fabricius <stosh.fabricius@gmail.com>
2018-07-17 14:37:46 -0400

    Update notes in jquery lecture

--- w03d2-breakout/code/example_3/main.js
+++ w03d2-breakout/code/example_3/main.js
@@ -1,8 +1,11 @@
 // Event Delegation
+// Allows us to attach a single event listener, to a parent element, 
+// that will fire for all descendants matching a selector, whether those descendants exist now or are added in the future.
 
-//     Event delegation allows us to attach a single event listener, to a parent element, that will fire for all descendants matching a selector, whether those descendants exist now or are added in the future.
+// The click event is the single event listener attached to the parent element of all of the <li> tags.
+// If I click on an <li> tag that is a descendant of the <ul> then it will call this function. 
+// This means I can add more <li> tags without needing to concern myself with adding done functionality to each one.
 
-// The click event is the single event listener attached to the parent element of all of the <li> tags. If I click on an <li> tag that is a descendant of the <ul> then it will call this function. This means I can add more <li> tags without needing to concern myself with adding done functionality to each one.
 
 $('ul').on('click', 'li.task', function(event) {
   var li = $(event.target)
