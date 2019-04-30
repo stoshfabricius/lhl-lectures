b4925a2ac4fb5e39bff749777da676fa7e43b28e
Stosh Fabricius <stosh.fabricius@gmail.com>
2019-04-30 15:00:05 -0400

    Update line length in w01d2 lecture

--- w01d2-breakout/code/functions_as_values.js
+++ w01d2-breakout/code/functions_as_values.js
@@ -1,14 +1,18 @@
 // Function declared as an expression
 // The function is anonymous as the function does not have a name
-// However, a name can be provided with a function expression and can be used inside the function to refer to itself, or in a debugger to identify the function in stack traces:
+// However, a name can be provided with a function expression 
+// and can be used inside the function to refer to itself,
+// or in a debugger to identify the function in stack traces:
 // These are convenient when passing a function as an argument to another function
+
 var add = function(a,b) {
   return a + b
 }
 console.log('Adding 5 + 6', add(5,6))
 
 
-// Function declared using function definition (also called function declaration, or function statement )
+// Function declared using function definition 
+// (also called function declaration, or function statement)
 function subtract(a, b) {
   return a - b
 }
@@ -18,4 +22,5 @@ console.log('Subtracting 3 -2 ', subtract(3,2))
 // What happens if we move the console.log above the function definition
 
 
-// Note: Function hoisting only works with functions created using function decleration, not function expressions
+// Note: Function hoisting only works with
+// functions created using function decleration, not function expressions
